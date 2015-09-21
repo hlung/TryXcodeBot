f2639faa1e9140e3989f813b1ddd13f498a3a4dc
Hlung <thongchaikol@gmail.com>
2015-09-21 16:52:07 +0700

    add failing test

--- TryXcodeBotTests/TryXcodeBotTests.m
+++ TryXcodeBotTests/TryXcodeBotTests.m
@@ -39,6 +39,11 @@
     XCTAssert(eq, @"Pass");
 }
 
+- (void)testFailingTest {
+    
+    XCTAssert(NO, @"Pass");
+}
+
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
